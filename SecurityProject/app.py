cbe9f27f0d43eba8aaae9c72d9c46a93866fb24e
abhay11242004 <abhaypatel274@gmail.com>
2024-12-09 20:07:59 -0500

    changed the app

--- SecurityProject/app.py
+++ SecurityProject/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from ciphers import encrypt_image, decrypt_image
+from flask import send_from_directory
 
 app = Flask(__name__)
 BASE_DIR = os.getcwd()
@@ -24,17 +25,24 @@ def hello_world():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(url_for('index'))
+@app.route('/upload/encrypt', methods=['POST'])
+def upload_encrypt():
     file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('index'))
-    if file:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(file_path)
-        return render_template('index.html', uploaded_file=file.filename)
+    filename = file.filename
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    return render_template('index.html', uploaded_encrypt_file=filename)
+
+
+@app.route('/upload/decrypt', methods=['POST'])
+def upload_decrypt():
+    file = request.files['file']
+    filename = file.filename
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    return render_template('index.html', uploaded_decrypt_file=filename)
 
 
 @app.route('/encrypt', methods=['POST'])
@@ -56,7 +64,17 @@ def decrypt():
     decrypted_img = decrypt_image(file_path, key)
     decrypted_path = os.path.join(app.config['DECRYPTED_FOLDER'], filename)
     decrypted_img.save(decrypted_path)
-    return render_template('index.html', decrypted_file=f'decrypted_{filename}')
+    return render_template('index.html', decrypted_file=filename)
+
+
+@app.route('/download/encrypted/<filename>')
+def download_encrypted(filename):
+    return send_from_directory(app.config['ENCRYPTED_FOLDER'], filename, as_attachment=True)
+
+
+@app.route('/download/decrypted/<filename>')
+def download_decrypted(filename):
+    return send_from_directory(app.config['DECRYPTED_FOLDER'], filename, as_attachment=True)
 
 
 if __name__ == '__main__':
